0281847aecf2c4e9e796148bdedea4fa48001cdb
nateoverd12 <nathan.pousset@club.fr>
2019-05-17 11:38:42 +0200

    Sf 13 EntityType # by_reference option git add . => fix the Sf 11

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Tag;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('tags', EntityType::class, [
+                'expanded'=>true,
+                'multiple'=>true,
+                'class'=>Tag::class,
+                'choice_label'=> 'name',
+                'by_reference' => false ] )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
