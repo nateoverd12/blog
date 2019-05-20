7f6aa2369aa0b8bb0057122f5f06bdcde14193e9
nateoverd12 <nathan.pousset@club.fr>
2019-05-20 17:15:04 +0200

    Fixtures done

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+use Faker;
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{   
+    public function getDependencies(): array
+    {
+        return [CategoryFixtures::class];
+    }
+
+    public function load(ObjectManager $manager): void
+    {   
+        for($i=0;$i<50;$i++){
+            $article = new Article();
+            $faker  =  Faker\Factory::create('fr_FR');
+            $article->setTitle(mb_strtolower($faker->sentence()));
+            $article->setContent($faker->sentence(10));
+
+            $manager->persist($article);
+            $article->setCategory($this->getReference('categorie_'.$faker->numberBetween(0,3)));
+        }
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    CONST CATEGORIES=[
+        'PHP',
+        'Java',
+        'C',
+        'Javascript'
+    ];
+
+    public function load(ObjectManager $manager)
+    {   
+        foreach (self::CATEGORIES as $key => $categoryName){
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+
+            $this->addReference('categorie_' . $key, $category);
+        }
+        $manager->flush();
+    }
+}
