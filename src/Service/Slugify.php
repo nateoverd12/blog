bb6e4d1d9fe8dc7200c9937a549bc5e60037e405
nateoverd12 <nathan.pousset@club.fr>
2019-05-29 16:45:05 +0200

    correct lil error w/ slugify line 10

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -7,6 +7,7 @@ class Slugify {
     {
         $input = html_entity_decode($input, ENT_QUOTES, 'UTF-8');
         // replace non letter or digits by -
+        $input = str_replace("'","",$input);
         $input = preg_replace('~[^\\pL\d.]+~u', '-', $input);
 
         // trim
