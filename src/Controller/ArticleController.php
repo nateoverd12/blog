b50f32bd094b295b600aba22471b83b7221e0076
nateoverd12 <nathan.pousset@club.fr>
2019-06-03 17:13:56 +0200

    sf quest Mailer, but without Service rule

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,7 +29,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/new", name="article_new", methods={"GET","POST"})
      */
-    public function new(Request $request,Slugify $slugify): Response
+    public function new(Request $request,Slugify $slugify, \Swift_Mailer $mailer): Response
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
@@ -39,8 +39,23 @@ class ArticleController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $article->setSlug($slugify->generate($article->getTitle()));
+
             $entityManager->persist($article);
             $entityManager->flush();
+       
+            $message = (new \Swift_Message('Un nouvel article vient d\'être publié !'))
+                ->setFrom($_ENV['MAILER_FROM_ADDRESS'])
+                // ->setTo('francois.doussin@wildcodeschool.fr')
+                ->setTo('nathan.dev.abroad@gmail.com')
+               ->setBody(
+                   $this->renderView('email/articleAdded.html.twig',
+                                    ['title' => $article->getTitle(),
+                                    'slug' => $article->getSlug()
+                                ]),
+                                    'text/html'
+               )
+            ;
+            $mailer->send($message);
 
             return $this->redirectToRoute('article_index');
         }
